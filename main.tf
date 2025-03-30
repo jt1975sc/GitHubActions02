27d81d37897eb9abae8b73f885fe51846fa4fcd7
JT-Butters <jt-butters@Butterss-Mac-mini-2.local>
2025-03-30 16:39:18 -0700

    added main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-2”  # Change to your AWS region
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = var.ami_id  # This is provided by the GitHub Action
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
