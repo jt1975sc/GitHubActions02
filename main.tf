eddaccfb97f3fa024b6eec9883b360a71d4d9885
jt1975sc <148169798+jt1975sc@users.noreply.github.com>
2025-03-30 21:24:46 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 provider "aws" {
-  region = "us-east-1”  # Change to your AWS region
+  region = "us-east-1"
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = var.ami_id  # This is provided by the GitHub Action
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-08b5b3a93ed654d19"  
   instance_type = "t2.micro"
+  key_name      = "AKIAQNO4QGTXRN7FZRDW" 
+  security_groups = ["my-security-group"] 
 
   tags = {
-    Name = "MyEC2Instance"
+    Name = "Terraform-EC2"
   }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.my_instance.public_ip
 }
